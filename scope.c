2796f7404669074c99fe472fa7cfa6142b764e87
Pankaj Dagade <106441405+PankajDagade@users.noreply.github.com>
2022-10-10 23:52:05 -0700

    Add files via upload

---
+++ scope.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+void fun1();  //prototype
+int a = 50;
+int main()
+{
+        int a = 1;
+        fun1();  // Function calling 
+        {
+                int a = 10;
+                printf("Inside block1 : %d\n",a);
+        }
+        {
+                printf("Inside block2 : %d\n",a++);
+        }
+        printf("Inside main %d",a);
+
+        return 0;
+}
+void fun1()  // function Defination
+{
+       // int a = 23;
+        printf("Inside fun1 : %d\n",a);
+}
