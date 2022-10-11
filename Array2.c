eaf75d9ecfbd93787a70719e27a351c0d6efa843
Pankaj Dagade <106441405+PankajDagade@users.noreply.github.com>
2022-10-11 09:48:41 -0700

    Add files via upload

---
+++ Array2.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main()
+{
+    int Data[7];
+
+    printf("Adrress of array is : %u\n",Data);
+
+    printf("Size of array is : %d\n",sizeof(Data));
+
+    return 0;
+}
+
+
+
+
+
+
+
+
