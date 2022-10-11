fc96670176b49e27bbc15485cc831117c66b7fcf
Pankaj Dagade <106441405+PankajDagade@users.noreply.github.com>
2022-10-11 11:48:47 -0700

    Add files via upload

---
+++ voidp.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+
+// #define NULL (void *)0
+
+#define PPA 15000
+
+int main()
+{
+    int *p = NULL;
+
+    int *q;
+
+    printf("%d",*q);
+
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
