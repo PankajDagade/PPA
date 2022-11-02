2dd7f7dd9cc5f45bcbf31a792bee670a5ba264db
Pankaj Dagade <106441405+PankajDagade@users.noreply.github.com>
2022-11-01 23:38:34 -0700

    Create while2.c

---
+++ while2.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+
+int main()
+{
+    int iCnt = 0;
+
+    //      1             2              3
+    for(iCnt = 0; iCnt < 5; iCnt++)
+    {
+        printf("Jay Ganesh\n");     // 4
+    }
+
+    return 0;
+}
+
+// 1    Loop counter initialization
+// 2    Loop condition (True/ False)
+// 3    Loop counter movement (Increment / Decrement)
+// 4    Loop body (Logic)
+
+
+// 1    2   4   3   2   4   3   2   4   3   2   4   3   2   4   3   2
+
+
+
+                        True                                        False
+
+
+
+
+
+
