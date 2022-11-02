8a190227a70cff215c9024f7dffa35c3928513d8
Pankaj Dagade <106441405+PankajDagade@users.noreply.github.com>
2022-11-01 23:28:38 -0700

    Create dynamic1.c

---
+++ dynamic1.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+    int size = 0;
+    int *ptr = NULL;
+
+    printf("Enter number of elements : ");
+    scanf("%d",&size);
+
+    ptr = (int *)malloc(size * sizeof(int));            // Step 1 : Allocate the memory
+    if(ptr == NULL)
+    {
+        printf("Unable to allocate memory\n");
+    }
+    else
+    {
+        printf("Memory succesfully allocated\n");
+    }
+
+    // Step 2: Use the memory
+
+    free(ptr);          // Step 3: Free the memory
+    return 0;
+}
+
+
+
+
+
+
