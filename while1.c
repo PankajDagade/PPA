e8f92b2ef9b5927486518492ac2e170cbcd1efa5
Pankaj Dagade <106441405+PankajDagade@users.noreply.github.com>
2022-11-01 23:37:49 -0700

    Create while1.c

---
+++ while1.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main()
+{
+    int iCnt = 0;       // 1
+
+    while(iCnt < 5)     // 2
+    {
+        printf("Jay Ganesh\n");     // 4
+        //iCnt++;     // 3
+    }
+
+    return 0;
+}
