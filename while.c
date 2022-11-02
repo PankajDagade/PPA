cdf346283cf8390b15c3b8845cffa1b991c56f69
Pankaj Dagade <106441405+PankajDagade@users.noreply.github.com>
2022-11-01 23:37:05 -0700

    Create while.c

---
+++ while.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main()
+{
+    // Sequance
+
+    printf("Jay Ganesh\n");
+    printf("Jay Ganesh\n");
+    printf("Jay Ganesh\n");
+    printf("Jay Ganesh\n");
+    printf("Jay Ganesh\n");
+
+    return 0;
+}
